96faaa76e78676835d782921865b25dca65664c3
M1loseph <milosz1998@onet.eu>
2020-11-11 16:07:05 +0100

    HTTP POST

--- Server/routes/handlers.js
+++ Server/routes/handlers.js
@@ -9,29 +9,79 @@ function getRooms(_, res) {
 }
 
 function getRoomData(req, res) {
-    const room_name = req.params.name;
+    const room_name = req.params.room;
     make_request(
-        `SELECT temperature, humidity, pressure
+        `SELECT temperature, humidity, pressure, time
          FROM WeatherReadings
-         WHERE room_name = ${room_name}`,
+         WHERE room_name = '${room_name}'`,
         res
     )
 }
 
 function getRoomTemperature(req, res) {
-    res.send(req.params);
+    const room_name = req.params.room;
+    make_request(
+        `SELECT temperature, time
+         FROM WeatherReadings
+         WHERE room_name = '${room_name}'`,
+        res
+    )
 }
 
 function getRoomHumidity(req, res) {
-    res.send(req.params);
+    const room_name = req.params.room;
+    make_request(
+        `SELECT humidity, time
+         FROM WeatherReadings
+         WHERE room_name = '${room_name}'`,
+        res
+    )
 }
 
 function getRoomPressure(req, res) {
-    res.send(req.params);
+    const room_name = req.params.room;
+    make_request(
+        `SELECT pressure, time
+         FROM WeatherReadings
+         WHERE room_name = '${room_name}'`,
+        res
+    )
 }
 
 function newData(req, res) {
-    res.send(req.body);
+    let { room_name, temperature, humidity, pressure } = req.body;
+
+    console.log('=========================');
+    console.log('Body: ');
+    console.log(req.body);
+    console.log('=========================');
+
+    if (room_name === undefined || temperature === undefined || humidity === undefined || pressure === undefined) {
+        res.status(400);
+        res.send("Not enough arguments");
+    } else {
+        temperature = parseFloat(temperature);
+        humidity = parseFloat(humidity);
+        pressure = parseFloat(pressure);
+
+        if (temperature === NaN || humidity === NaN || pressure === NaN) {
+            res.status(400);
+            res.send("Arguments are not numbers");
+        } else {
+            console.log('=========================');
+            console.log(room_name);
+            console.log(temperature);
+            console.log(humidity);
+            console.log(pressure);
+            console.log('=========================');
+            make_request(`
+                INSERT INTO WeatherReadings(room_name, temperature, humidity, pressure)
+                VALUES
+                ('${room_name}', ${temperature}, ${humidity}, ${pressure})
+            `,
+            res);
+        }
+    }
 }
 
 module.exports = {
