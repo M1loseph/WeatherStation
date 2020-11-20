e29539dfd23cd54d6ada4b9f17c2abf7fcf89716
M1loseph <milosz1998@onet.eu>
2020-11-20 19:04:18 +0100

    new db strucutre

--- Server/database/index.js
+++ Server/database/index.js
@@ -8,4 +8,71 @@ const pool = mariadb.createPool({
     connectionLimit: 5
 });
 
-module.exports = pool;
+function getWeather(room, success, error) {
+    pool.query(`
+        SELECT temperature, humidity, pressure, time
+        FROM WeatherReadings
+        WHERE room_id = 
+        (
+            SELECT room_id
+            FROM Rooms
+            WHERE room_name = ?
+        )`, [room])
+        .then(rows => {
+            success(rows);
+        })
+        .catch(err => {
+            error(err);
+        })
+}
+
+function getWeatherBetween(roomName, success, error, success, error) {
+    pool.query(`
+        SELECT temperature, humidity, pressure, time
+        FROM WeatherReadings
+        WHERE room_id = 
+        (
+            SELECT room_id
+            FROM Rooms
+            WHERE room_name = ?
+        )`, [roomName])
+        .then(rows => {
+            success(rows);
+        })
+        .catch(err => {
+            error(err);
+        })
+}
+
+function getRooms(success, error) {
+    pool.query('SELECT room_name FROM Rooms')
+        .then(rows => {
+            success(rows);
+        })
+        .catch(err => {
+            error(err);
+        })
+}
+
+function addWeatherReaging(room, temperature, humidity, pressure, success, error) {
+    pool
+        .query(`
+            INSERT INTO WeatherReadings(room_id, temperature, humidity, pressure)
+            VALUES
+            SELECT room_id, ?, ?, ? FROM Rooms
+            WHERE room_name = ?`,
+            [temperature, humidity, pressure, room])
+        .then(() => {
+            success();
+        })
+        .catch(err => {
+            error(err);
+        })
+}
+
+module.exports = {
+    getWeather: getWeather,
+    getWeatherBetween: getWeatherBetween,
+    getRooms: getRooms,
+    addWeatherReaging: addWeatherReaging,
+};
--- Server/routes/handlers.js
+++ Server/routes/handlers.js
@@ -1,77 +1,83 @@
-const pool = require('./../database')
+const chalk = require('chalk')
+const db = require('./../database')
 const validateDate = require('./validators')
 
+const clientErr = (message) => console.log(chalk.yellow.inverse(message));
+const serverErr = (message) => console.log(chalk.red.bold.inverse(message));
+
 function getRooms(_, res) {
-    pool
-        .query('SELECT room_name FROM Rooms')
-        .then(rows => {
+    db.getRooms(
+        (rows) => {
             res.send(rows);
-        })
-        .catch(err => {
+        },
+        (err) => {
+            serverErr(err);
             res.status(500);
             res.send('Server side error')
-        })
+        });
 }
 
 function getWeather(req, res) {
     const { room } = req.params;
-    pool
-        .query('SELECT temperature, humidity, pressure, time FROM WeatherReadings WHERE room_name = ?', [room])
-        .then(rows => {
-            res.send(rows);
-        })
-        .catch(err => {
-            res.status(500);
-            res.send('Server side error')
-        })
+    db.getWeather(room, (rows) => {
+        res.send(rows);
+    }, (err) => {
+        serverErr(err);
+        res.status(500);
+        res.send('Server side error')
+
+    });
 }
 
 function getWeatherBetween(req, res) {
     const { room, from, to } = req.params;
-    const from_parsed = validateDate(from);
-    const to_parsed = validateDate(to);
-    if(from_parsed !== undefined && to_parsed !== undefined) {
-        pool
-            .query('SELECT temperature, humidity, pressure, time FROM WeatherReadings WHERE room_name = ? AND time BETWEEN ? AND ?', [room, from_parsed, to_parsed])
-            .then(rows => {
+    const fromParsed = validateDate(from);
+    const toParsed = validateDate(to);
+    if (fromParsed !== undefined && toParsed !== undefined) {
+        db.getWeatherBetween(room, fromParsed, toParsed,
+            (rows) => {
                 res.send(rows);
-            })
-            .catch(err => {
+            },
+            (err) => {
+                serverErr(err);
                 res.status(500);
                 res.send('Server side error');
-            })
+            });
     } else {
+        clientErr(`[getWeatherBetween] Client arguments: ${from}, ${to}`);
         res.status(400);
         res.send('Incorrect date format');
     }
 }
 
 function newData(req, res) {
-    let { room_name, temperature, humidity, pressure } = req.body;
-    if (room_name === undefined || temperature === undefined || humidity === undefined || pressure === undefined) {
+    let { room, temperature, humidity, pressure } = req.body;
+    if (room === undefined) {
+        clientErr(`[newData] Client arguments: ${room}`)
         res.status(400);
-        res.send("Not enough arguments");
+        res.send("Room name must be privided");
     } else {
-        temperature = parseFloat(temperature);
-        humidity = parseFloat(humidity);
-        pressure = parseFloat(pressure);
+        temperature = temperature !== undefined ? parseFloat(temperature) : null;
+        humidity = humidity !== undefined ? parseFloat(humidity) : null;
+        pressure = pressure !== undefined ? parseFloat(pressure) : null;
 
         if (temperature === NaN || humidity === NaN || pressure === NaN) {
+            clientErr(`[newData] Client numbers: 
+                       temperature: ${temperature},
+                       humidity: ${humidity},
+                       pressure: ${[pressure]}`);
             res.status(400);
             res.send("Arguments are not numbers");
         } else {
-            pool
-                .query(`
-                INSERT INTO WeatherReadings(room_name, temperature, humidity, pressure)
-                VALUES (?, ?, ?, ?)`,
-                    [room_name, temperature, humidity, pressure])
-                .then(() => {
+            db.addWeatherReaging(room, temperature, humidity, pressure,
+                () => {
                     res.status(200);
-                    res.send("OK");
-                })
-                .catch(err => {
-                    res.status(400);
-                    res.send("Querry error, problably this room name does not exist")
+                    res.send('OK');
+                },
+                (err) => {
+                    serverErr(err);
+                    res.send(500);
+                    res.send('Server side error');
                 })
         }
     }
