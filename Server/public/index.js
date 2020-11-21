e37a210913204b87e2608dcc9a04b902c70bf7f2
M1loseph <milosz1998@onet.eu>
2020-11-21 09:26:26 +0100

    ofc i forgot to change localhost

--- Server/public/index.js
+++ Server/public/index.js
@@ -3,7 +3,7 @@ let humidityChart = null;
 let pressureChart = null;
 
 function showData(room, from, to, showTemperature, showHumidity, showPressure) {
-    fetch(`http://localhost/api/weather/${room}/${from}/${to}`)
+    fetch(`http://172.16.1.69/api/weather/${room}/${from}/${to}`)
         .then(response => response.json())
         .then(json => {
 
