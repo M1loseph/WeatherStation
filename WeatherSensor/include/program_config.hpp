2a24e8bf7866178a1a4e01d0f5808804c9e09996
M1loseph <milosz1998@onet.eu>
2020-11-25 21:50:34 +0100

    new build

--- WeatherSensor/include/program_config.hpp
+++ WeatherSensor/include/program_config.hpp
@@ -7,6 +7,10 @@
 #error Wrong build flags!
 #endif
 
+#if (defined(MILOSZ) && defined(SEBA)) || (!defined(MILOSZ) && !defined(SEBA))
+#error Only one name can be specified!
+#endif 
+
 #ifdef ESP8266
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
@@ -15,22 +19,32 @@
 #include <HTTPClient.h>
 #endif
 
+static constexpr uint8_t get_diode_pin()
+{
+#ifdef ESP8266
+    return D5;
+#elif defined(ESP32)
+    return 33U
+#endif
+}
+
+static constexpr const char* get_room_name()
+{
+#ifdef MILOSZ
+    return "milosz";
+#elif defined(MILOSZ)
+    return "seba";
+#endif
+}
+
 namespace config
 {
     constexpr uint32_t signalise_threshold = 20U;
     constexpr unsigned long monitor_speed = 115200;
-
-#ifdef ESP8266
-    constexpr uint8_t diode_pin = D5;
-    constexpr uint64_t sleep_time = 30e7;
-    constexpr uint64_t sleep_time_on_error = 10e8;
-    constexpr const char *room_name = "seba";
-#elif defined(ESP32)
-    constexpr uint8_t diode_pin = 33;
-    constexpr uint64_t sleep_time = 10e7;
+    constexpr uint8_t diode_pin = get_diode_pin();
+    constexpr uint64_t sleep_time = 20e7;
     constexpr uint64_t sleep_time_on_error = 10e7;
-    constexpr const char *room_name = "milosz";
-#endif
+    constexpr const char *room_name = get_room_name();
 } // namespace config
 
 #endif // __PROGRAM_CONFIG_HPP__
